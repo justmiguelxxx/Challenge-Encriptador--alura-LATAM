902e81a9abc534e9f28fb3412ede44def986f19b
miguel <miguelgolindano1987@gmail.com>
2023-01-23 21:24:33 -0400

    first upload alura LATAM

---
+++ readme.txt
@@ -0,0 +1,4 @@
+pagina web desarrollada por Miguel Golindano utilizando los conocimientos
+adquiridos mediante el curso de principiante de programacion de alura latam
+
+miguelgolindano1987@gmail.com
---
+++ script.js
@@ -0,0 +1,61 @@
+const textArea = document.querySelector(".textareainput");
+const mensaje = document.querySelector(".mensaje");
+
+
+
+
+function buttonencriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+function buttondesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+
+function encriptar(stringEncriptada){
+
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada
+}
+
+function desencriptar(stringDesencriptada){
+
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada
+}
+
+function copybutton(){
+    // Get the text field
+    var copyText = document.getElementById("copiar");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
